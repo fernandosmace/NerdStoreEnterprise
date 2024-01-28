5317fcc6ee1fef25bbb4626c6ee2584cc546b0ae
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2024-01-27 21:48:54 -0300

    Add config for Swagger

---
+++ src/services/NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NSE.Identidade.API.Configuration;
+
+public static class SwaggerConfig
+{
+    public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+    {
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "NerdStore Enterprise Identity API",
+                Description = "API para realização de autenticação na aplicação NerdStore Enterprise",
+                Contact = new OpenApiContact() { Name = "Fernando Macedo", Email = "contato@nerdstoreenterprise.com.br" },
+                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+            })
+        );
+
+        return services;
+    }
+}
