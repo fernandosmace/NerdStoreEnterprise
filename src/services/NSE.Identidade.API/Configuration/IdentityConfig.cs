77bdae3f629325c2cf854270acfe5dcb4e879586
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2024-01-27 21:48:48 -0300

    Add config for Identity

---
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
+
+namespace NSE.Identidade.API.Configuration;
+
+public static class IdentityConfig
+{
+    public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddDefaultIdentity<IdentityUser>()
+            .AddRoles<IdentityRole>()
+            .AddErrorDescriber<IdentityMessagesPortuguese>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        var appSettingsSection = configuration.GetSection("AppSettings");
+        services.Configure<AppSettings>(appSettingsSection);
+
+        var appSettings = appSettingsSection.Get<AppSettings>();
+        services.AddSingleton<AppSettings>(appSettings);
+
+        var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(bearerOptions =>
+        {
+            bearerOptions.RequireHttpsMetadata = true;
+            bearerOptions.SaveToken = true;
+            bearerOptions.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings.ValidIn,
+                ValidIssuer = appSettings.Issuer
+            };
+        });
+
+        return services;
+    }
+}
